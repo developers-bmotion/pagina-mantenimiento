bde0517ea27acb0487a8def98ea59921ac1e8cd9
Mauricio Gutierrez <developer.mauricio2310@gmail.com>
2020-05-24 10:16:08 -0500

    name

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 	TITLE AND BASIC META TAGS
 	=============================================== -->
     <meta charset="utf-8">
-    <title>Rosa - Responsive Coming Soon Template</title>
+    <title>{{ config('app.name') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="keywords" content="">
